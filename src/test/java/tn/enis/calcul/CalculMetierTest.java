9b07e8ca1bb4b664f73e67f997a3b0e680123b2f
Iyed Rouabeh <mohamed-iyed.rouabeh@enis.tn>
2025-11-26 11:15:19 +0100

    Initial commit du projet CalculMaven

---
+++ src/test/java/tn/enis/calcul/CalculMetierTest.java
@@ -0,0 +1,19 @@
+package tn.enis.calcul;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculMetierTest {
+
+    @Test
+    public void testSomme() {
+        CalculMetier metier = new CalculMetier();
+        assertTrue(metier.somme(9, 10) == 19, "La somme est fausse");
+    }
+
+    @Test
+    public void testProduit() {
+        CalculMetier metier = new CalculMetier();
+        assertTrue(metier.produit(9, 10) == 90, "Le produit est faux");
+    }
+}
